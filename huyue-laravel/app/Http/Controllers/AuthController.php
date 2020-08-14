8af9ed5b8b66de0bb0a96bd97be749ca8f90db39
Rotor1996 <605297441@qq.com>
2020-08-14 07:41:07 +0000

    完善注册，登陆

--- huyue-laravel/app/Http/Controllers/AuthController.php
+++ huyue-laravel/app/Http/Controllers/AuthController.php
@@ -5,9 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\RegisterUserRequest;
 use App\Models\User;
+use GuzzleHttp\Client as Guzzle;
 
 class AuthController extends Controller
 {
+    public function __construct(Guzzle $http)
+    {
+        $this->http = $http;
+    }
     //注册账号
     public function register(RegisterUserRequest $request){
         //写入数据库
@@ -16,9 +21,20 @@ class AuthController extends Controller
             'email'     =>  $request->email,
             'password'  =>  bcrypt($request->password)
         ]);
+
+        $response = $this->http->post('http://huyue.test/oauth/token', [
+            'form_params' => [
+                'grant_type'    =>  'password',
+                'client_id'     =>  '2',
+                'client_secret' =>  'E73wI07kluRlLRbDSJgojXGA4rTSvP8qfc4ORLsO',
+                "username"      =>  $request->email,
+                "password"      =>  $request->password,
+                'scope'         =>  '*'
+            ],
+        ]);
+        $token = json_decode((string) $response->getBody(), true);
         return response()->json([
-            'code'=>'200',
-            'data'=>'注册成功'
-        ],200);
+            'token'=>$token
+        ],201);
     }
 }
